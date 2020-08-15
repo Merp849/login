81c95bef4f20900332d0dcd00d710e80945dc815
Merp849 <59752534+Merp849@users.noreply.github.com>
2020-08-15 15:03:04 +0700

    Update log.py

--- log.py
+++ log.py
@@ -6,13 +6,9 @@ def menu():
       while True:
            print("")
            os.system("clear")
-           print('')
-           os.system('date | lolcat')
            print("\033[1;93m")
-           print(" \033[1;92m Please login to continue!")
+           print(" \033[1;92mPlease login to continue!")
            print("\033[1;93m")
-
-           print("")
            try:
                 x = str(input('\033[1;92mUsername \033[1;93m: '))
                 e = getpass('\033[1;92mPassword \033[1;93m: ')
@@ -26,29 +22,14 @@ def menu():
                    print("")
                    break
                 else:
-                      print("")
-                      print("")
-                      print("")
-                      print("")
                       print("\033[1;91m Password / Username is wrong!")
                       time.sleep(2)
                       print("")
            except Exception:
-                      
-                      print("")
-                      print("")
-                      print("")
-                      print("")
-                      print("")
                       print("\033[1;91m Password / Username is wrong!")
                       time.sleep(2)
            except KeyboardInterrupt:
-                      print("")
                       os.system('killall -9 com.termux')
-                      print("")
-                      print("")
-                      print("")
-                      print("")
                       print("\033[1;91m Password / Username is wrong!")
                       time.sleep(2)
 menu()
